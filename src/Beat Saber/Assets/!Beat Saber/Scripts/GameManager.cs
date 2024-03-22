df8c319431571d2fcdf4d3205d0f0159c0e3dc7b
ShutovKS <schutow.kir@yandex.ru>
2024-03-23 00:44:13 +0300

    Add namespace in scripts

--- src/Beat Saber/Assets/!Beat Saber/Scripts/GameManager.cs
+++ src/Beat Saber/Assets/!Beat Saber/Scripts/GameManager.cs
@@ -3,104 +3,107 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+namespace Beat_Saber.Scripts
 {
-    public static GameManager Instance { get; private set; }
-
-    public Action OnGameOver;
-    public event Action<int> OnScoreChanged;
-    public event Action<int> OnHealthChanged;
-
-    [SerializeField, Header("Ссылка на источник звука")] 
-    private AudioSource audioSource;
-
-    [field: Space, SerializeField, Header("Игровые данные")] 
-    public GameData GameData { get; private set; }
-
-    private bool _isDead;
-    private int _score;
-    private int _health;
-
-    private void Awake()
+    public class GameManager : MonoBehaviour
     {
-        InitializedSingleton();
+        public static GameManager Instance { get; private set; }
 
-        OnScoreChanged += score =>
+        public Action OnGameOver;
+        public event Action<int> OnScoreChanged;
+        public event Action<int> OnHealthChanged;
+
+        [SerializeField, Header("Ссылка на источник звука")] 
+        private AudioSource audioSource;
+
+        [field: Space, SerializeField, Header("Игровые данные")] 
+        public GameData GameData { get; private set; }
+
+        private bool _isDead;
+        private int _score;
+        private int _health;
+
+        private void Awake()
         {
-            if (GameData.IsDebug)
+            InitializedSingleton();
+
+            OnScoreChanged += score =>
             {
-                Debug.Log($"Score: {score}");
-            }
-        };
+                if (GameData.IsDebug)
+                {
+                    Debug.Log($"Score: {score}");
+                }
+            };
 
-        OnHealthChanged += health =>
-        {
-            if (GameData.IsDebug)
+            OnHealthChanged += health =>
             {
-                Debug.Log($"Health: {health}");
-            }
-        };
-    }
-
-    private void Start()
-    {
-        _health = GameData.HealthOnStart;
-        OnHealthChanged?.Invoke(_health);
-
-        _score = 0;
-        OnScoreChanged?.Invoke(_score);
-        
-        if (audioSource != null)
-        {
-            StartCoroutine(AudioClipOnComplete());
+                if (GameData.IsDebug)
+                {
+                    Debug.Log($"Health: {health}");
+                }
+            };
         }
-    }
 
-    public void OnHitCorrectSaber()
-    {
-        _score++;
-        OnScoreChanged?.Invoke(_score);
-    }
-
-    public void OnHitDestroy()
-    {
-        _health--;
-        OnHealthChanged?.Invoke(_health);
-
-        if (_health <= 0 && !_isDead)
+        private void Start()
         {
+            _health = GameData.HealthOnStart;
+            OnHealthChanged?.Invoke(_health);
+
+            _score = 0;
+            OnScoreChanged?.Invoke(_score);
+        
+            if (audioSource != null)
+            {
+                StartCoroutine(AudioClipOnComplete());
+            }
+        }
+
+        public void OnHitCorrectSaber()
+        {
+            _score++;
+            OnScoreChanged?.Invoke(_score);
+        }
+
+        public void OnHitDestroy()
+        {
+            _health--;
+            OnHealthChanged?.Invoke(_health);
+
+            if (_health <= 0 && !_isDead)
+            {
+                StartCoroutine(GameOver());
+            }
+        }
+
+        private IEnumerator GameOver()
+        {
+            _isDead = true;
+            OnGameOver?.Invoke();
+
+            yield return new WaitForSeconds(0.5f);
+
+            SceneManager.LoadScene(0);
+        }
+
+        private IEnumerator AudioClipOnComplete()
+        {
+            var clipLength = audioSource.clip.length;
+
+            yield return new WaitForSeconds(clipLength);
+        
             StartCoroutine(GameOver());
         }
-    }
 
-    private IEnumerator GameOver()
-    {
-        _isDead = true;
-        OnGameOver?.Invoke();
-
-        yield return new WaitForSeconds(0.5f);
-
-        SceneManager.LoadScene(0);
-    }
-
-    private IEnumerator AudioClipOnComplete()
-    {
-        var clipLength = audioSource.clip.length;
-
-        yield return new WaitForSeconds(clipLength);
-        
-        StartCoroutine(GameOver());
-    }
-
-    private void InitializedSingleton()
-    {
-        if (Instance == null)
+        private void InitializedSingleton()
         {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
--- src/Beat Saber/Assets/!Beat Saber/Scripts/StatisticsUI.cs
+++ src/Beat Saber/Assets/!Beat Saber/Scripts/StatisticsUI.cs
@@ -1,18 +1,21 @@
 using TMPro;
 using UnityEngine;
 
-public class StatisticsUI : MonoBehaviour
+namespace Beat_Saber.Scripts
 {
-    [SerializeField, Header("Ссылка на текст где будет отображаться количество жизней")]
-    private TextMeshProUGUI healthText;
-
-    [SerializeField, Header("Ссылка на текст где будет отображаться количество очков")]
-    private TextMeshProUGUI scoreText;
-
-    private void Start()
+    public class StatisticsUI : MonoBehaviour
     {
-        GameManager.Instance.OnHealthChanged += health => healthText.text = $"{health}";
+        [SerializeField, Header("Ссылка на текст где будет отображаться количество жизней")]
+        private TextMeshProUGUI healthText;
 
-        GameManager.Instance.OnScoreChanged += score => scoreText.text = $"{score}";
+        [SerializeField, Header("Ссылка на текст где будет отображаться количество очков")]
+        private TextMeshProUGUI scoreText;
+
+        private void Start()
+        {
+            GameManager.Instance.OnHealthChanged += health => healthText.text = $"{health}";
+
+            GameManager.Instance.OnScoreChanged += score => scoreText.text = $"{score}";
+        }
     }
 }
